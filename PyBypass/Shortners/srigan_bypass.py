08a95c7defa8730b71402c9aeb9b8f1429ac9877
sαɴᴊɪᴛ sɪɴʜα <sanjitsinha@anipirates.eu.org>
2022-10-16 01:00:32 +0530

    Update srigan_bypass.py

--- PyBypass/Shortners/srigan_bypass.py
+++ PyBypass/Shortners/srigan_bypass.py
@@ -2,16 +2,22 @@ import requests
 from base64 import b64decode
 
 
+"""
+https?://(safeurl\.sirigan\.my\.id\/)\S+
+https://safeurl.sirigan.my.id/proses.html?asu=WVVoU01HTkViM1pNTWxZeFRHMDVlVnAzUFQwPQ=
+"""
+
 def srigan_myid_bypass(srigan_link:str) -> str:
     
     client = requests.Session()
     response = client.get(srigan_link)
     
     url = response.url.split('=', maxsplit=1)[-1]
+    url = b64decode(url).decode('utf-8')
 
-   while True:
+    while True:
         try: url = b64decode(url).decode('utf-8')
         except: break
-
-   return url.split('url=')[-1]
-
+    return url.split('url=')[-1]
+   
+   
