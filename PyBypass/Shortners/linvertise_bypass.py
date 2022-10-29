4d335b6c8951874636639753f4bfb74faddc39d8
sαɴᴊɪᴛ sɪɴʜα <sanjitsinha@anipirates.eu.org>
2022-10-29 17:13:17 +0530

    Update linvertise_bypass.py

--- PyBypass/Shortners/linvertise_bypass.py
+++ PyBypass/Shortners/linvertise_bypass.py
@@ -1,13 +1,18 @@
 import requests 
 
+"""
 
-#Linvertise_domains = "linkvertise.com", "linkvertise.net", "up-to-down.net", "link-to.net", "direct-link.net", "linkvertise.download", "file-link.net", "link-center.net", "link-target.net", "link-hub.net"
+https://linkvertise.com/417174/mommy/1
+https?://(linkvertise\.com/)\S+
+Linvertise subdomains = ["linkvertise.com", "linkvertise.net", "up-to-down.net", "link-to.net", "direct-link.net", "linkvertise.download", "file-link.net", "link-center.net", "link-target.net", "link-hub.net"]
+"""
 
-def linvertise_bypass(linvertise_url: str) -> str:
+def linvertise_bypass(url: str) -> str:
 	
 	linvertise_bypass_api = "https://bypass.bot.nu/bypass2?url="
-	try: response = requests.get(linvertise_bypass_api+linvertise_url).json()["destination"]
+	try: response = requests.get(linvertise_bypass_api+url).json()["destination"]
 	except: return None
 	
 	return response
+	
 	
