4ff42c7a75a4d4aaccbc88844765b87746b99eab
sαɴᴊɪᴛ sɪɴʜα <sanjitsinha@anipirates.eu.org>
2022-10-29 22:16:17 +0530

    Update and rename sourceforge.py to sourceforge_bypass.py

--- PyBypass/Filehoster/sourceforge_bypass.py
+++ PyBypass/Filehoster/sourceforge_bypass.py
@@ -8,7 +8,7 @@ from bs4 import BeautifulSoup
 https://sourceforge.net/projects/derpfestveux/files/DerpFest-13-Community-Tango-veux-20221004-2258.zip/download
 """
 
-def sourceforge(url: str) -> str:
+def sourceforge_bypass(url: str) -> str:
     link = re.findall(r"\bhttps?://sourceforge\.net\S+", url)[0]
     file_path = re.findall(r"files(.*)/download", link)[0]
     project = re.findall(r"projects?/(.*?)/files", link)[0]
