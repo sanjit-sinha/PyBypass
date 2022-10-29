bcc331755eb228a88e3e9ac991307db659e08d23
sαɴᴊɪᴛ sɪɴʜα <sanjitsinha@anipirates.eu.org>
2022-10-29 22:14:54 +0530

    Update and rename sfile.py to sfile_bypass.py

--- PyBypass/Filehoster/sfile_bypass.py
+++ PyBypass/Filehoster/sfile_bypass.py
@@ -7,7 +7,7 @@ https?://(sfile\.mobi/)\S+
 https://sfile.mobi/1nNX2v3K5iI7
 """
 
-def sfile(url:str) -> str:
+def sfile_bypass(url:str) -> str:
     headers = {
         "User-Agent": "Mozilla/5.0 (Linux; Android 8.0.1; SM-G532G Build/MMB29T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3239.83 Mobile Safari/537.36"
     }
