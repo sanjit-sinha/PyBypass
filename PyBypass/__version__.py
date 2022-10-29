19c0b4392d7256286308367cbebcdd031b6b915b
sαɴᴊɪᴛ sɪɴʜα <sanjitsinha@anipirates.eu.org>
2022-10-30 04:04:59 +0530

    Create __version__.py

---
+++ PyBypass/__version__.py
@@ -0,0 +1,10 @@
+
+
+__title__ = "PyBypass"
+__description__ = "Python module of bypassing different kind of links."
+#__url__ = ""
+__version__ = "1.0.0"
+__author__ = "sanjit sinha"
+__author_email__ = "sanjitsinha@anipirates.eu.org"
+#__license__ = ""
+#__copyright__ = ""
