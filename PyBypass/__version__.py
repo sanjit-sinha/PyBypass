ef513598de60d18f86742b4575dcd69d09840af6
sαɴᴊɪᴛ sɪɴʜα <sanjitsinha@anipirates.eu.org>
2022-10-30 12:21:33 +0530

    Update __version__.py

--- PyBypass/__version__.py
+++ PyBypass/__version__.py
@@ -1,8 +1,8 @@
 __title__ = "PyBypass"
-__description__ = "Python module of bypassing different kind of links."
-#__url__ = ""
-__version__ = "1.0.0"
+__description__ = "Python module for bypassing different kind of links."
+__version__ = "1.0"
 __author__ = "sanjit sinha"
 __author_email__ = "sanjitsinha@anipirates.eu.org"
-#__license__ = ""
-#__copyright__ = ""
+__license__ = "MIT"
+__copyright__ = "sanjitsinha 2022"
+
