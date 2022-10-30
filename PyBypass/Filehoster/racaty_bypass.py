77d3047d449c4d5cd5f113c500f7027d56deb198
sαɴᴊɪᴛ sɪɴʜα <sanjitsinha@anipirates.eu.org>
2022-10-31 04:25:53 +0530

    Update racaty_bypass.py

--- PyBypass/Filehoster/racaty_bypass.py
+++ PyBypass/Filehoster/racaty_bypass.py
@@ -31,7 +31,7 @@ def racaty_bypass(url: str)-> str:
 
         response = client.post(url, headers=headers, data=data)
         soup = BeautifulSoup(response.text, "html.parser")  
-        print(
+       
 
         if (btn := soup.find(class_="btn btn-dow")): return btn["href"]
         if (unique := soup.find(id="uniqueExpirylink")): return unique["href"]                                                         
