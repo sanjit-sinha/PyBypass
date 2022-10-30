a617d3d960a15a2224562a61c5c9929063534c82
sαɴᴊɪᴛ sɪɴʜα <sanjitsinha@anipirates.eu.org>
2022-10-30 18:01:59 +0530

    Update __init__.py

--- PyBypass/__init__.py
+++ PyBypass/__init__.py
@@ -1,2 +1,2 @@
-from .__version__ import(__author__, __author_email__, __copyright__, __version__, __title__, __description__, __license__)
-from .main import bypass
+from PyBypass.__version__ import(__author__, __author_email__, __copyright__, __version__, __title__, __description__, __license__)
+from PyBypass.main import bypass
