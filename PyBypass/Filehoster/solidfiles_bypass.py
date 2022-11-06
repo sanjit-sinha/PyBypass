f8089f6d956bfc95d1d04fb534b2e069125660a5
sαɴᴊɪᴛ sɪɴʜα <sanjitsinha@anipirates.eu.org>
2022-11-07 01:03:00 +0530

    Update solidfiles_bypass.py

--- PyBypass/Filehoster/solidfiles_bypass.py
+++ PyBypass/Filehoster/solidfiles_bypass.py
@@ -8,9 +8,9 @@ https?://(www\.solidfiles\.com/v/)\S+
 http://www.solidfiles.com/v/WqL3V2QjdmDZv
 """
 
-def solidfiles_bypass(solidfiles_url: str) -> str:
-    
-   json_file = re.search(r'viewerOptions\'\,\ (.*?)\)\;', requests.get(solidfiles_url).content)[1]
-   return json.loads(json_file)["downloadUrl"]
-    
+def solidfiles_bypass(url: str) -> str:
+    json_file = re.search(r"'viewerOptions\'\,\ (.*?)\)\;", requests.get(url).text)
+    download_url = json.loads(json_file.group(1))["downloadUrl"]
+    return download_url
    
+    
