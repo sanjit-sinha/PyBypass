fdd67123b6b62cd24d292db534b9847173799d3b
sαɴᴊɪᴛ sɪɴʜα <sanjitsinha@anipirates.eu.org>
2022-11-07 04:31:48 +0530

    Imporved custom errors.

--- PyBypass/main.py
+++ PyBypass/main.py
@@ -8,7 +8,7 @@ from PyBypass.VideoServers import *
 
 class BypasserNotFoundError(Exception):
 	"""
-	Raise when there is no bypasser of the giver url.
+	Raise when there is no bypasser script for the provided URL.
 	"""
 		
 
@@ -27,14 +27,14 @@ class RequiredValueNotFoundError(Exception):
 class UnableToBypassError(Exception):
 	"""
 	Raise when script is unable to bypass the given url. 
-	( possible reason can be wrong link, wrong parameters or script is patched)
+	( possible reason can be wrong link, wrong parameters, cloudfare protection or script is patched)
 	"""
 
 		
 def _requiredvaluechecker(function):
 	
 	"""
-	a decorator to check if required parameters is/are present or not  to bypass 
+	A decorator to check if required parameters is/are present or not  to bypass 
 	the given links
 	"""
 	
@@ -43,17 +43,17 @@ def _requiredvaluechecker(function):
 		func_name = args[-1]
 		if func_name == "gdtot_bypass":	
 			if ("gdtot_crypt" in kwargs) == False:
-				raise RequiredValueNotFoundError(" Missing required parameter 'gdtot_crypt'. please enter yout GDOT crypt value")
+				raise RequiredValueNotFoundError("Missing required parameter 'gdtot_crypt'. please enter your GDOT crypt value")
 			
 				
 		if func_name =="appdrive_bypass":	
 			if all([("appdrive_email" in kwargs ), ("appdrive_password" in kwargs)])== False:	
-				raise RequiredValueNotFoundError("Missing required parameter 'appdrive_email' and 'appdrive_password. please enter yout appdrive credential value to bypass the given link.")
+				raise RequiredValueNotFoundError("Missing required parameter 'appdrive_email' and 'appdrive_password. please enter yout appdrive credentials to bypass the given link.")
 				
 				
 		if func_name =="hubdrive_bypass":			
 			if ("hubdrive_crypt" in kwargs) == False:
-				raise RequiredValueNotFoundError(" Missing required parameter 'hubdrive_crypt'. please enter your hubdrive crypt value")
+				raise RequiredValueNotFoundError("Missing required parameter 'hubdrive_crypt'. please enter your hubdrive crypt value")
 		
 				
 		if func_name =="sharerpw_bypass":			
@@ -84,11 +84,11 @@ class PyBypass:
 		try:
 			bypassed_value = eval(bypasser_function + f"('{url}'{parameter})")
 		except Exception as e:
-			raise UnableToBypassError("Can not bypass the given url. possible reason can be wrong link, wrong parameters or script is patched")
+			raise UnableToBypassError("Can not able to bypass this link. Possible reason can be cloudfare protection, wrong link, wrong parameters, expired  link or script is patched")
 	
 		
 		if bypassed_value == None:
-		    raise UnableToBypassError("Can not bypass the given url. possible reason can be wrong link, wrong parameters or script is patched")
+		    raise UnableToBypassError("Can not able to bypass this link. Possible reason can be cloudfare protection, wrong link, wrong parameters, expired  link or script is patched")
 		return bypassed_value
 		 
 		 
@@ -103,10 +103,10 @@ class PyBypass:
 				response = requests.get(url)
 				
 			except Exception as e:
-				raise UrlConnectionError("Can not establish a successful connection with the given url.")
+				raise UrlConnectionError("Not able to establish a successful connection with given URL. It is probably protected by cloudfare.")
 			
 			if response.status_code != 200:
-				raise UrlConnectionError("Can not establish a successful connection with the given url.")
+				raise UrlConnectionError("Not able to establisha a successful connection with given URL. It is probably protected by cloudfare.")
 	
 	
 		bypasser_function = None
@@ -124,10 +124,9 @@ class PyBypass:
 		if bypasser_function is not None:
 			return self.redirect_function(url, bypasser_function, **kwargs)
 		else:
-			raise BypasserNotFoundError("Can not find any bypasser script for the given url")
+			raise BypasserNotFoundError("can not find a bypasser script found for this URL.")
 
 
 def bypass(url, name=None, **kwargs):
 	bypasser = PyBypass()
 	return bypasser.bypass(url , name=name, **kwargs)
-	
