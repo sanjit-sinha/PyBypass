d31c421250b8792ced66ffe550c09ff051a311bc
sαɴᴊɪᴛ sɪɴʜα <sanjitsinha@anipirates.eu.org>
2022-11-11 08:38:05 +0530

    2.0.0 -> 2.0.1
    Better Guide update.

--- PyBypass/__version__.py
+++ PyBypass/__version__.py
@@ -1,6 +1,6 @@
 __title__ = "PyBypass"
 __description__ = "Python module for bypassing different kind of links."
-__version__ = "2.0.0"
+__version__ = "2.0.1"
 __author__ = "Sanjit Sinha"
 __author_email__ = "sanjitsinha@anipirates.eu.org"
 __license__ = "MIT"
